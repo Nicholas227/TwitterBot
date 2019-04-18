135eaf479f3294eb1adbbc910c9af165da177819
Nick Wille <43426629+Nicholas227@users.noreply.github.com>
2019-04-18 16:02:54 -0500

    Add files via upload

--- src/GetTweetData.java
+++ src/GetTweetData.java
@@ -1,1 +1,92 @@
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+        Main class for TwitterBot sends the tweet
+ */
+public class GetTweetData {
+    public static void main(String[] args) {
+        ConfigurationBuilder config = new ConfigurationBuilder();
+        config.setOAuthConsumerKey("DTzbV5XE0IENqeQg1FM3RopDD");
+        config.setOAuthConsumerSecret("hvMtr5CUxJtfr73sCxrqfpA74wSMvE4oZ3xedUxxHRWpeqFtGx");
+        config.setOAuthAccessToken("1063205806525558784-PVTGKaNqjLUkgypzM0R7eG2DTa9UDu");
+        config.setOAuthAccessTokenSecret("skIiGVCMHhyK6hr2yPcpNUoybFryedJnJrC1AD5SRODAS");
+
+        TwitterFactory twitFac = new TwitterFactory();
+        twitter4j.Twitter twit = twitFac.getInstance();
+        twit.tweets();
+        try {
+            String test2 = "";
+            ResponseList<Status> listOfMentions = twit.getMentionsTimeline();
+            for (Status s : listOfMentions) {
+                System.out.println("Status " + s);
+                User m = s.getUser();
+                test2 = m.getScreenName();
+                    writeToFile(m, twit);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Unable to continue, see stacktrace");
+                }
+            }
+            System.out.println(test2);
+        } catch (TwitterException e) {
+            System.out.println("There is something wrong with the twitter object");
+            e.printStackTrace();
+        }
+        try {
+            TwitterBot theBot = new TwitterBot();
+            MarkovObject temp = new MarkovObject();
+            temp.analyzeText();
+            temp.startNLP();
+
+//            twit.updateStatus(theBot.makeTweet());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void writeToFile(User theUser, Twitter twit) {
+        try {
+            String userName = theUser.getScreenName();
+            FileWriter f = new FileWriter("/Users/nick/IdeaProjects/TwitterBot/src/main/resources/userTweetData.txt");
+            BufferedWriter bf = new BufferedWriter((f));
+            bf.write("@" + userName + "\n");
+            int numStatus = theUser.getStatusesCount();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Unable to continue, see stacktrace");
+            }
+            try {
+                TwitterFactory ok = new TwitterFactory();
+                twitter4j.Twitter twit2 = ok.getInstance();
+                ResponseList<Status> userStatus = twit.getUserTimeline(userName);
+                for (Status s : userStatus) {
+                   String[] a =  s.getText().trim().split(" ");
+                    for(int i = 0; i < a.length; i++){
+                        if(!a[i].contains("@")){
+                            bf.write(a[i] + " ");
+                        }
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.out.println("Unable to continue, see stacktrace");
+                    }
+                }
+                bf.close();
+                f.close();
+            } catch (TwitterException e) {
+                //oops
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/MarkovObject.java
@@ -0,0 +1,267 @@
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.CoreNLPProtos;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/*
+    Class by Nick Wille, generates a markov object, step lenth of 1,
+    Meant for twitter so length of markov chain is capped at 260 chars
+ */
+public class MarkovObject {
+
+    private String markovChain;
+    private Map markovMap;
+    private CoreNLPProtos.Sentiment theSentiment;
+    private ArrayList<Integer> sentimentScores;
+
+    /*
+            Generates a markov chain from an existing file on my computer, markov step length of 1
+     */
+    public void analyzeText() throws IOException {
+        final int MAX_NUM_CHARS = 260;
+
+        //initialization of variables for future use
+        Map<String, ArrayList<String>> theData = new HashMap<String, ArrayList<String>>();
+        Path thePath = Paths.get("/Users/nick/IdeaProjects/TwitterBot/src/main/resources/UserTweetData.txt");
+        byte[] theBytes = Files.readAllBytes(thePath);
+
+        //splits data file, stores an array of string seperated by spaces
+        String[] splitWords = new String(theBytes).trim().split(" ");
+        String userName = splitWords[0];
+
+        //loops through array of string
+        for (int i = 1; i < splitWords.length - 2; i++) {
+
+            //lines 38-53 put the word in the hashmap, but also check, to comply with
+            //twitter guidelines and not be a nuisance, any strings using the '@' char
+            //that could be tagging other users are avoided
+            String tempString = splitWords[i];
+
+            //nested conditionals the avoid tagging others can be removed with future iterations of main class
+            if (!tempString.contains('@' + "")) {
+                if (theData.containsKey(tempString)) {
+                    if(splitWords[i + 1].contains("@"))
+                        theData.get(tempString).add(splitWords[i + 2]);
+                    else
+                        theData.get(tempString).add(splitWords[i + 1]);
+                } else {
+                    ArrayList<String> words = new ArrayList<>();
+                    if(!splitWords[i + 1].contains("@"))
+                        words.add(splitWords[i + 1]);
+                    else
+                        words.add(splitWords[i + 2]);
+                    theData.put(tempString, words);
+                }
+            }
+        }
+
+        //Initializes markov chain
+        StringBuilder sb = new StringBuilder();
+        sb.append(userName);
+        sb.append(" ");
+        Random random = new Random();
+        int charCount = userName.length() + 1;
+        String currentWord = splitWords[random.nextInt(splitWords.length)];
+
+        //regenerates initial word to avoid tagging calling user twice
+        while(currentWord.contains("@")){
+            currentWord = splitWords[random.nextInt(splitWords.length)];
+        }
+
+        //appends the next words in the generated text
+        while (charCount < MAX_NUM_CHARS) {
+            if (theData.get(currentWord) == null) {
+                currentWord = splitWords[random.nextInt(splitWords.length)];
+            }
+            int num = random.nextInt(theData.get(currentWord).size());
+            String nextWord = theData.get(currentWord).get(num);
+            sb.append(nextWord);
+            sb.append(" ");
+
+            charCount += nextWord.length() + 1;
+            currentWord = nextWord;
+        }
+        markovMap = theData;
+       markovChain = sb.toString();
+
+    }
+
+    //getter method for the chain
+    public String getMarkovChain(){
+        return markovChain;
+    }
+
+    //regenerates the chain
+    public void regenerateChain() throws IOException{
+        analyzeText();
+    }
+
+    //initializes the chain and map to any data file
+    public MarkovObject() throws IOException {
+        try {
+            analyzeText();
+        } catch (Exception e){
+            //do nothing,
+        }
+    }
+
+    //initializes the chain from a given data file
+    public MarkovObject(File file) throws IOException {
+        try {
+            analyzeText(file);
+        } catch (Exception e){
+            //do nothing,
+        }
+    }
+
+    //returns the map for debugging
+    public Map debugMap(){
+        return markovMap;
+    }
+
+    /*
+            Generates a markov chain from an existing text file, markov step length of 1
+     */
+    public void analyzeText(File file) throws IOException {
+        final int MAX_NUM_CHARS = 260;
+
+        //initialization of variables for future use
+        Map<String, ArrayList<String>> theData = new HashMap<String, ArrayList<String>>();
+        Path thePath = Paths.get("/Users/nick/IdeaProjects/TwitterBot/src/main/resources/UserTweetData.txt");
+        byte[] theBytes = Files.readAllBytes(thePath);
+
+        //splits data file, stores an array of string seperated by spaces
+        String[] splitWords = new String(theBytes).trim().split(" ");
+        String userName = splitWords[0];
+
+        //loops through array of string
+        for (int i = 1; i < splitWords.length - 2; i++) {
+
+            //lines 38-53 put the word in the hashmap, but also check, to comply with
+            //twitter guidelines and not be a nuisance, any strings using the '@' char
+            //that could be tagging other users are avoided
+            String tempString = splitWords[i];
+
+            //nested conditionals the avoid tagging others can be removed with future iterations of main class
+            if (!tempString.contains('@' + "")) {
+                if (theData.containsKey(tempString)) {
+                    if(splitWords[i + 1].contains("@"))
+                        theData.get(tempString).add(splitWords[i + 2]);
+                    else
+                        theData.get(tempString).add(splitWords[i + 1]);
+                } else {
+                    ArrayList<String> words = new ArrayList<>();
+                    if(!splitWords[i + 1].contains("@"))
+                        words.add(splitWords[i + 1]);
+                    else
+                        words.add(splitWords[i + 2]);
+                    theData.put(tempString, words);
+                }
+            }
+        }
+
+        //Initializes markov chain
+        StringBuilder sb = new StringBuilder();
+        sb.append(userName);
+        sb.append(" ");
+        Random random = new Random();
+        int charCount = userName.length() + 1;
+        String currentWord = splitWords[random.nextInt(splitWords.length)];
+
+        //regenerates initial word to avoid tagging calling user twice
+        while(currentWord.contains("@")){
+            currentWord = splitWords[random.nextInt(splitWords.length)];
+        }
+
+        //appends the next words in the generated text
+        while (charCount < MAX_NUM_CHARS) {
+            if (theData.get(currentWord) == null) {
+                currentWord = splitWords[random.nextInt(splitWords.length)];
+            }
+            int num = random.nextInt(theData.get(currentWord).size());
+            String nextWord = theData.get(currentWord).get(num);
+            sb.append(nextWord);
+            sb.append(" ");
+
+            charCount += nextWord.length() + 1;
+            currentWord = nextWord;
+        }
+        markovMap = theData;
+        markovChain = sb.toString();
+    }
+
+    public String getSentiment(){
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        String sentimentOverall = "";
+        for(int i = 0; i < sentimentScores.size(); i++){
+            num += sentimentScores.get(i);
+        }
+        if(num == 0){
+             sentimentOverall = "neutral";
+        } else {
+            sentimentOverall = num > 0 ? "happy" : "unhappy";
+        }
+        sb.append(", generally, your tweets are ");
+        sb.append( sentimentOverall);
+        sb.append(".");
+        return sb.toString();
+    }
+
+    public void startNLP() throws IOException {
+        Properties attributes = new Properties();
+        ArrayList<Integer> sentiments = new ArrayList<>();
+        attributes.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP pipe = new StanfordCoreNLP(attributes);
+        Map<String, ArrayList<String>> theData = new HashMap<String, ArrayList<String>>();
+        Path thePath = Paths.get("/Users/nick/IdeaProjects/TwitterBot/src/main/resources/UserTweetData.txt");
+        byte[] theBytes = Files.readAllBytes(thePath);
+        String stringOfText = new String(theBytes);
+        Annotation annotation = new Annotation(stringOfText);
+        pipe.annotate(annotation);
+        for(CoreMap s : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+            Tree annotatedTree = s.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+            int score = RNNCoreAnnotations.getPredictedClass(annotatedTree);
+            sentiments.add(score);
+        }
+        sentimentScores = sentiments;
+    }
+
+    public String getSentimentTrajectory() {
+        final int QUADRANT_NUMBER  = 4;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Your tweets are getting ");
+        int[] theScores = new int[QUADRANT_NUMBER];
+        for(int i = 0; i < QUADRANT_NUMBER; i++) {
+            for(int j = 0; j < sentimentScores.size()/QUADRANT_NUMBER; j++){
+                theScores[i] += sentimentScores.get(j + i * QUADRANT_NUMBER);
+            }
+        }
+        if(theScores[1] < theScores[2] && theScores[2] < theScores[3] && theScores[3] < theScores[4){
+            sb.append("always happier!");
+        } else
+        if(theScores[1] > theScores[2] && theScores[2] < theScores[3] && theScores[3] < theScores[4){
+            sb.append("recently happier!");
+        } else
+        if(theScores[1] > theScores[2] && theScores[2] > theScores[3] && theScores[3] > theScores[4){
+            sb.append("always less happy...");
+        } else
+        if(theScores[1] > theScores[2] && theScores[2] < theScores[3] && theScores[3] > theScores[4){
+            sb.append("mostly unhappy...");
+        } else {
+            sb.append("emotionally confusing...");
+        }
+        return sb.toString();
+    }
+}
